6c241b2d4f542a74223bc1a96dd23f26412371ab
Bertrand Juglas <b@juglas.name>
2019-06-23 11:59:55 +0200

    finish

---
+++ ngo-rest-api/README.md
@@ -0,0 +1,232 @@
+# Part 3: RESTful API to expose the Chaincode
+
+The RESTful API is a Node.js application that uses the Fabric SDK to interact with the Fabric chaincode
+and exposes the chaincode functions as REST APIs. This allows loose coupling between the UI application
+and the underlying Fabric network.
+
+## Pre-requisites
+For the Fabric workshop, the REST API server will run on the Fabric client node.
+
+From Cloud9, SSH into the Fabric client node. The key (i.e. the .PEM file) should be in your home directory. 
+The DNS of the Fabric client node EC2 instance can be found in the output of the AWS CloudFormation stack you 
+created in [Part 1](../ngo-fabric/README.md)
+
+```
+ssh ec2-user@<dns of EC2 instance> -i ~/<Fabric network name>-keypair.pem
+```
+
+You should have already cloned this repo in [Part 1](../ngo-fabric/README.md)
+
+```
+cd ~
+git clone https://github.com/aws-samples/non-profit-blockchain.git
+```
+
+You will need to set the context before carrying out any Fabric CLI commands. We do this 
+using the export files that were generated for us in [Part 1](../ngo-fabric/README.md)
+
+Source the file, so the exports are applied to your current session. If you exit the SSH 
+session and re-connect, you'll need to source the file again. The `source` command below
+will print out the values of the key ENV variables. Make sure they are all populated. If
+they are not, follow Step 4 in [Part 1](../ngo-fabric/README.md) to repopulate them:
+
+```
+cd ~/non-profit-blockchain/ngo-fabric
+source fabric-exports.sh
+```
+
+## Step 1 - Install Node
+On the Fabric client node.
+
+Install Node.js. We will use v8.x.
+
+```
+curl -o- https://raw.githubusercontent.com/creationix/nvm/v0.33.0/install.sh | bash
+```
+
+```
+. ~/.nvm/nvm.sh
+nvm install lts/carbon
+nvm use lts/carbon
+```
+
+Amazon Linux seems to be missing g++, so:
+
+```
+sudo yum install gcc-c++ -y
+```
+
+## Step 2 - Install dependencies
+On the Fabric client node.
+
+```
+cd ~/non-profit-blockchain/ngo-rest-api
+npm install
+```
+
+## Step 3 - Generate a connection profile
+On the Fabric client node.
+
+The REST API needs a connection profile to connect to the Fabric network. Connection profiles describe
+the Fabric network and provide information needed by the Node.js application in order to connect to the
+Fabric network. The instructions below will auto-generate a connection profile. 
+
+Generate the connection profile using the script below and check that the connection profile contains 
+URL endpoints for the peer, ordering service and CA, an 'mspid', a 'caName', and that the admin username and password
+match those you entered when creating the Fabric network. If they do not match, edit the connection profile
+and update them. The connection profile can be found here: `~/non-profit-blockchain/tmp/connection-profile/ngo-connection-profile.yaml`
+
+```
+cd ~/non-profit-blockchain/ngo-rest-api/connection-profile
+./gen-connection-profile.sh
+more ~/non-profit-blockchain/tmp/connection-profile/ngo-connection-profile.yaml
+cd ~/non-profit-blockchain/tmp/connection-profile/
+ln -s ngo-connection-profile.yaml client-.yaml
+```
+
+Check the config file used by app.js. Make sure the peer name in config.json (under 'peers:') is 
+the same as the peer name in the connection profile. Also check that the admin username and 
+password are correct and match the values you updated in the connection profile.
+
+```
+cd ~/non-profit-blockchain/ngo-rest-api
+vi config.json
+```
+
+config.json should look something like this:
+
+```
+{
+    "host":"localhost",
+    "port":"3000",
+    "channelName":"mychannel",
+    "chaincodeName":"ngo",
+    "eventWaitTime":"30000",
+    "peers":[
+        "peer1"
+    ],
+    "admins":[
+       {
+          "username":"admin",
+          "secret":"Adminpwd1!"
+       }
+    ]
+ }
+```
+
+## Step 4 - Run the REST API Node.js application
+On the Fabric client node.
+
+Run the app (in the background if you prefer):
+
+```
+cd ~/non-profit-blockchain/ngo-rest-api
+nvm use lts/carbon
+node app.js &
+```
+
+## Step 5 - Test the REST API
+On the Fabric client node.
+
+Once the app is running you can register an identity, and then start to execute chaincode. The command
+below registers a user identity with the Fabric CA. This user identity is then used to run chaincode
+queries and invoke transactions.
+
+### Register/enroll a user:
+
+request:
+```
+curl -s -X POST http://localhost:3000/users -H "content-type: application/x-www-form-urlencoded" -d 'username=michael&orgName=Org1'
+```
+
+response:
+```
+{"success":true,"secret":"","message":"michael enrolled Successfully"}
+```
+
+### POST a Donor
+
+request:
+```
+curl -s -X POST "http://localhost:3000/donors" -H "content-type: application/json" -d '{ 
+   "donorUserName": "edge2", 
+   "email": "edge2@def.com", 
+   "registeredDate": "2018-10-22T11:52:20.182Z" 
+}'
+```
+
+response:
+A transaction ID, which can be ignored:
+
+```
+{"transactionId":"2f3f3a85340bde09b505b0d37235d1d32a674e43a66229f9a205e7d8d5328ed1"}
+```
+
+### Get all donors
+
+request:
+```
+curl -s -X GET   "http://localhost:3000/donors" -H "content-type: application/json"
+```
+
+response:
+```
+[
+    {"docType":"donor","donorUserName":"edge","email":"edge@def.com","registeredDate":"2018-10-22T11:52:20.182Z"}
+]
+```
+## Step 6 - Load the workshop test data
+In your Cloud9 terminal.
+
+You can do this step from anywhere as it accesses the ELB DNS endpoint. Executing this from the SSH
+session is challenging as the SSH session will be outputting a range of INFO logs, which makes it
+challenging to edit files. So you can open another terminal window in Cloud9 and load the test data
+from Cloud9.
+
+Loading the test data uses cURL commands similar to those you used above to test the API. If you load
+the test data from Cloud9 you'll need to point to the AWS Elastic Load Balancer (ELB) that is used to 
+expose your REST API (if you load the test data from your Fabric client node you could use 'localhost'
+as the endpoint since the REST API server is running on the Fabric client node). To find the 
+DNS endpoint for the ELB, go to the CloudFormation stack created in [Part 1](../ngo-fabric/README.md)
+and look for ELBDNS in the Outputs. If you receive an error using the ELB it might be because the underlying EC2 
+instance has not moved to an 'InService' state. This will happen once the REST API server is running
+and the ELB is able to execute the desired number of health checks against it. You can check the 
+status in the EC2 console, under Load Balancers.
+
+```
+cd ~/non-profit-blockchain/ngo-rest-api
+vi ngo-load-workshop.sh
+```
+
+The line to be changed is this one. It should point to your ELB DNS. (it could point to `localhost` 
+if you run this on the Fabric client node. If you use `localhost` you also need to change the port to `3000`):
+
+```
+export ENDPOINT=ngo10-elb-2090058053.us-east-1.elb.amazonaws.com
+export PORT=80
+```
+
+After saving the changes, run the script:
+
+```
+cd ~/non-profit-blockchain/ngo-rest-api
+./ngo-load-workshop.sh
+```
+
+# Testing
+The workshop runs the REST API server on the Fabric client node. If you exit the SSH session on the Fabric client node, 
+the running REST API server will automatically exit. You would need to restart it after SSH'ing back into 
+the Fabric client node.
+
+For purposes of the workshop we can just leave the SSH session open. However, if we need to keep the REST 
+API application running after we exit the SSH session, we can use various methods to do this. I use `PM2`,
+using a command such as `pm2 start app.js`, which will keep the app running. The logs can be found in `~/.pm2/logs`.
+
+## Move on to Part 4
+The workshop instructions can be found in the README files in parts 1-4:
+
+* [Part 1:](../ngo-fabric/README.md) Start the workshop by building the Hyperledger Fabric blockchain network using Amazon Managed Blockchain.
+* [Part 2:](../ngo-chaincode/README.md) Deploy the non-profit chaincode. 
+* [Part 3:](../ngo-rest-api/README.md) Run the RESTful API server. 
+* [Part 4:](../ngo-ui/README.md) Run the application. 
+* [Part 5:](../new-member/README.md) Add a new member to the network. 
---
+++ ngo-rest-api/app.js
@@ -0,0 +1,214 @@
+/*
+# Copyright 2018 Amazon.com, Inc. or its affiliates. All Rights Reserved.
+#
+# Licensed under the Apache License, Version 2.0 (the "License").
+# You may not use this file except in compliance with the License.
+# A copy of the License is located at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# or in the "license" file accompanying this file. This file is distributed
+# on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either
+# express or implied. See the License for the specific language governing
+# permissions and limitations under the License.
+#
+*/
+
+'use strict';
+var log4js = require('log4js');
+log4js.configure({
+	appenders: {
+	  out: { type: 'stdout' },
+	},
+	categories: {
+	  default: { appenders: ['out'], level: 'info' },
+	}
+});
+var logger = log4js.getLogger('NGOAPI');
+const WebSocketServer = require('ws');
+var express = require('express');
+var bodyParser = require('body-parser');
+var http = require('http');
+var util = require('util');
+var app = express();
+var cors = require('cors');
+var hfc = require('fabric-client');
+const uuidv4 = require('uuid/v4');
+
+var connection = require('./connection.js');
+var query = require('./query.js');
+var invoke = require('./invoke.js');
+var blockListener = require('./blocklistener.js');
+
+hfc.addConfigFile('config.json');
+var host = 'localhost';
+var port = 3000;
+var username = "";
+var orgName = "";
+var channelName = hfc.getConfigSetting('channelName');
+var chaincodeName = hfc.getConfigSetting('chaincodeName');
+var peers = hfc.getConfigSetting('peers');
+///////////////////////////////////////////////////////////////////////////////
+//////////////////////////////// SET CONFIGURATIONS ///////////////////////////
+///////////////////////////////////////////////////////////////////////////////
+app.options('*', cors());
+app.use(cors());
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({
+	extended: false
+}));
+app.use(function(req, res, next) {
+	logger.info(' ##### New request for URL %s',req.originalUrl);
+	return next();
+});
+
+//wrapper to handle errors thrown by async functions. We can catch all
+//errors thrown by async functions in a single place, here in this function,
+//rather than having a try-catch in every function below. The 'next' statement
+//used here will invoke the error handler function - see the end of this script
+const awaitHandler = (fn) => {
+	return async (req, res, next) => {
+		try {
+			await fn(req, res, next)
+		}
+		catch (err) {
+			next(err)
+		}
+	}
+}
+
+///////////////////////////////////////////////////////////////////////////////
+//////////////////////////////// START SERVER /////////////////////////////////
+///////////////////////////////////////////////////////////////////////////////
+var server = http.createServer(app).listen(port, function() {});
+logger.info('****************** SERVER STARTED ************************');
+logger.info('***************  Listening on: http://%s:%s  ******************',host,port);
+server.timeout = 240000;
+
+function getErrorMessage(field) {
+	var response = {
+		success: false,
+		message: field + ' field is missing or Invalid in the request'
+	};
+	return response;
+}
+
+///////////////////////////////////////////////////////////////////////////////
+//////////////////////////////// START WEBSOCKET SERVER ///////////////////////
+///////////////////////////////////////////////////////////////////////////////
+const wss = new WebSocketServer.Server({ server });
+wss.on('connection', function connection(ws) {
+	logger.info('****************** WEBSOCKET SERVER - received connection ************************');
+	ws.on('message', function incoming(message) {
+		console.log('##### Websocket Server received message: %s', message);
+	});
+
+	ws.send('something');
+});
+
+///////////////////////////////////////////////////////////////////////////////
+///////////////////////// REST ENDPOINTS START HERE ///////////////////////////
+///////////////////////////////////////////////////////////////////////////////
+// Health check - can be called by load balancer to check health of REST API
+app.get('/health', awaitHandler(async (req, res) => {
+	res.sendStatus(200);
+}));
+
+// Register and enroll user. A user must be registered and enrolled before any queries
+// or transactions can be invoked
+app.post('/users', awaitHandler(async (req, res) => {
+	logger.info('================ POST on Users');
+	username = req.body.username;
+	orgName = req.body.orgName;
+	logger.info('##### End point : /users');
+	logger.info('##### POST on Users- username : ' + username);
+	logger.info('##### POST on Users - userorg  : ' + orgName);
+	let response = await connection.getRegisteredUser(username, orgName, true);
+	logger.info('##### POST on Users - returned from registering the username %s for organization %s', username, orgName);
+    logger.info('##### POST on Users - getRegisteredUser response secret %s', response.secret);
+    logger.info('##### POST on Users - getRegisteredUser response secret %s', response.message);
+    if (response && typeof response !== 'string') {
+        logger.info('##### POST on Users - Successfully registered the username %s for organization %s', username, orgName);
+		logger.info('##### POST on Users - getRegisteredUser response %s', response);
+		// Now that we have a username & org, we can start the block listener
+		await blockListener.startBlockListener(channelName, username, orgName, wss);
+		res.json(response);
+	} else {
+		logger.error('##### POST on Users - Failed to register the username %s for organization %s with::%s', username, orgName, response);
+		res.json({success: false, message: response});
+	}
+}));
+
+/************************************************************************************
+ * Geolocation methods
+ ************************************************************************************/
+
+// GET a specific Parcel
+app.get('/parcels/:parcelId', awaitHandler(async (req, res) => {
+	logger.info('================ GET on Parcel by ID');
+	logger.info('Parcel id : ' + req.params);
+	let args = req.params;
+	let fcn = "getParcelLocation";
+
+    logger.info('##### GET on Parcel by username - username : ' + username);
+	logger.info('##### GET on Parcel by username - userOrg : ' + orgName);
+	logger.info('##### GET on Parcel by username - channelName : ' + channelName);
+	logger.info('##### GET on Parcel by username - chaincodeName : ' + chaincodeName);
+	logger.info('##### GET on Parcel by username - fcn : ' + fcn);
+	logger.info('##### GET on Parcel by username - args : ' + JSON.stringify(args));
+	logger.info('##### GET on Parcel by username - peers : ' + peers);
+
+    let message = await query.queryChaincode(peers, channelName, chaincodeName, args, fcn, username, orgName);
+ 	res.send(message);
+}));
+
+// POST Parcel
+app.post('/parcels', awaitHandler(async (req, res) => {
+	logger.info('================ POST on Parcel');
+	var args = req.body;
+	var fcn = "storeGeoLocation";
+
+  logger.info('##### POST on Parcel - username : ' + username);
+	logger.info('##### POST on Parcel - userOrg : ' + orgName);
+	logger.info('##### POST on Parcel - channelName : ' + channelName);
+	logger.info('##### POST on Parcel - chaincodeName : ' + chaincodeName);
+	logger.info('##### POST on Parcel - fcn : ' + fcn);
+	logger.info('##### POST on Parcel - args : ' + JSON.stringify(args));
+	logger.info('##### POST on Parcel - peers : ' + peers);
+
+	let message = await invoke.invokeChaincode(peers, channelName, chaincodeName, args, fcn, username, orgName);
+	res.send(message);
+}));
+
+/************************************************************************************
+ * Blockchain metadata methods
+ ************************************************************************************/
+
+// GET details of a blockchain transaction using the record key (i.e. the key used to store the transaction
+// in the world state)
+app.get('/blockinfos/:docType/keys/:key', awaitHandler(async (req, res) => {
+	logger.info('================ GET on blockinfo');
+	logger.info('Key is : ' + req.params);
+	let args = req.params;
+	let fcn = "queryHistoryForKey";
+
+	logger.info('##### GET on blockinfo - username : ' + username);
+	logger.info('##### GET on blockinfo - userOrg : ' + orgName);
+	logger.info('##### GET on blockinfo - channelName : ' + channelName);
+	logger.info('##### GET on blockinfo - chaincodeName : ' + chaincodeName);
+	logger.info('##### GET on blockinfo - fcn : ' + fcn);
+	logger.info('##### GET on blockinfo - args : ' + JSON.stringify(args));
+	logger.info('##### GET on blockinfo - peers : ' + peers);
+
+	let history = await query.queryChaincode(peers, channelName, chaincodeName, args, fcn, username, orgName);
+	logger.info('##### GET on blockinfo - queryHistoryForKey : ' + util.inspect(history));
+	res.send(history);
+}));
+
+/************************************************************************************
+ * Error handler
+ ************************************************************************************/
+
+app.use(function(error, req, res, next) {
+	res.status(500).json({ error: error.toString() });
+});
